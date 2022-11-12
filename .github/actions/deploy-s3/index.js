7afbf3f2abed44abaa7dc5e6d3d34d01e1793cae
marius-lionte <mariusmihai.learning@gmail.com>
2022-11-12 21:31:18 +0200

    deploy app to s3

--- .github/actions/deploy-s3/index.js
+++ .github/actions/deploy-s3/index.js
@@ -1,8 +1,24 @@
 const core = require("@actions/core");
+const exec = require("@actions/exec");
 // const github = require("@actions/github");
 
 function run() {
-  core.notice("Hello from my custom Javascript Action");
+  const bucketName = core.getInput("bucket-name", {
+    trimWhitespace: true,
+    required: true
+  });
+  const bucketRegion = core.getInput("bucket-region", {
+    required: true,
+    trimWhitespace: true
+  });
+  const appFolder = core.getInput("app-folder", {
+    required: true,
+    trimWhitespace: true
+  });
+
+  exec.exec(
+    `aws s3 sync ${appFolder} s3://${bucketName} --delete --region ${bucketRegion}`
+  );
 }
 
 run();
